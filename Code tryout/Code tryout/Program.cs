0cbedb7fca6028ec5dfc5c3715a3ca9c43171851
VictorESR <32260113+VictorESR@users.noreply.github.com>
2019-02-20 20:22:11 +0100

    INSANE CODE SKILLZ
    BOI

---
+++ Code tryout/Code tryout/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+                                    using System.Collections.Generic;
+                                    using System.Linq;
+                    using System.Text;
+using System.Threading.Tasks;
+
+                                    namespace Code_tryout
+{
+class Program
+{
+static void Main(string[] args)
+                {
+while (true)
+            {
+                int Punan = 2;
+                                            Console.WriteLine("Hi there, I can make magic with numbers");
+Console.WriteLine("Please specify the first number you want to use (Keep in mind, I also take numbers as words or Roman numerals. I am also limited to numbers between 1-10. You can not mix uppercase words, lowercase words, numbers and roman numerals");
+                             int Füllen = 3;
+                                              int SteamGamesAndStore = 6;
+           string ПеРвАЯсУммА;
+                int Напълнете = 0;
+        ПеРвАЯсУммА = Console.ReadLine();
+                     int مبلغجديد;
+                int Riempire = 0;
+                if (ПеРвАЯсУммА == "One")                   {مبلغجديد = 1;} else                            if (ПеРвАЯсУммА == "one")
+                                            {                    مبلغجديد = 1; }else            if (ПеРвАЯсУммА == "I")
+                {
+                    مبلغجديد = 1;
+                }
+     else   if (ПеРвАЯсУммА == "Two")
+                {                    مبلغجديد = 2; //CODE TIME
+                }
+                                                                                else    if (ПеРвАЯсУммА == "V")
+      {
+                    مبلغجديد = 5;
+                } //TEA TIME?
+                                                else                if (ПеРвАЯсУммА == "eight"){ مبلغجديد = 8;}else if (ПеРвАЯсУммА == "X")  {
+                    مبلغجديد = 10;
+                }
+                                        else                    if (ПеРвАЯсУммА == "two")
+                                    {
+                                                 مبلغجديد = 2;
+                                                 }      //lmao
+        else                        if (ПеРвАЯсУммА == "II")
+                {                    مبلغجديد = 2;}else                 if (ПеРвАЯсУммА == "Three"){    مبلغجديد = 3;}else if (ПеРвАЯсУммА == "VI") {
+                    مبلغجديد = 6;
+                                         }
+                                                                                else if (ПеРвАЯсУммА == "three")
+                        {
+                               مبلغجديد = 3;
+                                                    }
+                                         else if (ПеРвАЯсУммА == "VIIII")
+                        {
+                                                                 مبلغجديد = 9;
+                                                      }
+                                                                                    else if (ПеРвАЯсУммА == "III"){
+                                    مبلغجديد = 3;
+                } //GOTTA. GO FUCKING FASTER
+  else if (ПеРвАЯсУммА == "seven")
+                                {
+                                                              مبلغجديد = 7;
+                                                     }
+                                            else if (ПеРвАЯсУммА == "four"){مبلغجديد = 4;}else if (ПеРвАЯсУммА == "IIII"){مبلغجديد = 4;}else if (ПеРвАЯсУммА == "Five")
+                            {
+                                      مبلغجديد = 5;
+                            }   //BOI
+                                                    else if (ПеРвАЯсУммА == "five")
+                            {
+               مبلغجديد = 5;
+                }
+                                        else if (ПеРвАЯсУммА == "Six")
+                                   {
+                                          مبلغجديد = 6;
+                                      }
+                                                else if (ПеРвАЯсУммА == "six")
+                      {
+                                                                         مبلغجديد = 6;
+                             }
+                
+             else if (ПеРвАЯсУммА == "Seven")
+    {   //lmao
+                                                             مبلغجديد = 7;
+                }
+                                  else                  if (ПеРвАЯсУммА == "Four")
+                                 {
+                                                                           مبلغجديد = 4;
+                                         }
+                                   else if (ПеРвАЯсУммА == "VII")
+                                                 {
+                                                                                    مبلغجديد = 7;
+                                     }
+                                                                           else if (ПеРвАЯсУммА == "Eight")
+                                                                          {
+                    مبلغجديد = 8;
+                                                  }
+                
+                                                                        else            if (ПеРвАЯсУммА == "VIII")
+                                {
+    مبلغجديد = 8;
+                }
+                                    else if (ПеРвАЯсУммА == "Nine")
+                                        {
+                    مبلغجديد = 9;
+                                                        }
+                                                                 else if (ПеРвАЯсУммА == "nine")
+                                        {
+                                               مبلغجديد = 9;
+                              }
+                
+                  else if (ПеРвАЯсУммА == "Ten")
+            {
+                                                      مبلغجديد = 10;
+                              }
+                                  else if (ПеРвАЯсУммА == "ten")
+                                     {
+                                                 مبلغجديد = 10;
+                                     }
+                                                           else
+                                  {
+                                                        مبلغجديد = int.Parse(ПеРвАЯсУммА);
+                                  }Console.WriteLine("Please specify the second number you want to use");
+                                                                        string LAsEgunDASuMa;
+     LAsEgunDASuMa = Console.ReadLine();
+                int Tổngmới;
+                                                              if (LAsEgunDASuMa == "One")
+          {
+                                                     Tổngmới = 1;
+                                     } 
+                                                                                                      else if (LAsEgunDASuMa == "Two")
+                                                                                     {
+                                                                                                   Tổngmới = 2;
+                                    }
+                //xD
+      else if (LAsEgunDASuMa == "Three")
+                                                                             {
+                                                    Tổngmới = 3;
+                                             }
+                                                  else if (LAsEgunDASuMa == "three")
+                                                                                                      {
+                                                                             Tổngmới = 3;
+                                                 }
+                                                                                                     else if (LAsEgunDASuMa == "eight")
+                                          {
+         Tổngmới = 8;
+                                                                                  }
+                                                       else if (LAsEgunDASuMa == "X")
+                                               {
+                                                                                               Tổngmới = 10;
+                                                                  }
+                                                                            else if 
+                    (LAsEgunDASuMa == "III")
+                                                                                                                    {
+                                                                                Tổngmới = 3;
+                                                                            }
+         else                    if                                                                 (LAsEgunDASuMa == "Four")
+                                                     {
+                                                                                                 Tổngmới = 4;
+                                     }
+                                     else                        if (LAsEgunDASuMa == "seven")
+                                                                    {
+                                                                                            Tổngmới = 7;
+                }
+                                             else                                            if (LAsEgunDASuMa == "VIIII")
+                                {
+                                                                Tổngmới = 9;
+                                        }
+                                                                          else if (LAsEgunDASuMa == "IIII")
+       {
+                                                                             Tổngmới = 4; }
+                                                                else                    if (LAsEgunDASuMa == "one")
+                                                                 {
+                                                                    Tổngmới = 1;
+        }
+                                                                                      else if (LAsEgunDASuMa == "nine")
+                {
+                                         Tổngmới = 9;
+                }
+
+
+                else if                     (LAsEgunDASuMa == "five")
+                {
+                                      Tổngmới = 5;
+                }
+                                                                                             else if (LAsEgunDASuMa == "II")
+                                                                      {
+                                                      Tổngmới = 2;
+                                                                          }
+                                                                                     else if (LAsEgunDASuMa == "I")
+                                                                          {
+                                        Tổngmới = 1;
+                                                                }
+                                                        else if (LAsEgunDASuMa == "V")
+                                             {
+                                                                                             Tổngmới = 5;
+                              }
+                else if                                     (LAsEgunDASuMa == "Six")
+                                                                                                     {
+                    Tổngmới = 6;
+                }
+        else                            if (LAsEgunDASuMa == "two")
+                                         {
+                                                                        Tổngmới = 2;
+                                    }
+
+                                                                            else if (LAsEgunDASuMa == "VI")
+                              {
+                                                                                                                  Tổngmới = 6;
+                                                                                     }
+                                 else                               if (LAsEgunDASuMa == "Seven")
+                                             {
+                                                                     Tổngmới = 7;
+                              }
+                                         else if (LAsEgunDASuMa == "Ten")
+                                                                             {
+                                              Tổngmới = 10;
+                                                                       }                else if (LAsEgunDASuMa == "Five"){    Tổngmới = 5;}
+                                                                                                                    else if (LAsEgunDASuMa == "VII")
+                                                {
+                            Tổngmới = 7;
+                }
+                                 else if (LAsEgunDASuMa == "Eight")
+                                {
+                                                     Tổngmới = 8;
+                              }
+               
+                                                                            else if             (LAsEgunDASuMa == "VIII")
+                                                                                                            {
+                                                                                                Tổngmới = 8;
+                                                                            }
+                                else                        if (LAsEgunDASuMa == "six")
+                                                                         {
+                                                Tổngmới = 6;
+                                }
+                else if                         (LAsEgunDASuMa == "four")
+                                         {
+                                             Tổngmới = 4;
+                              }
+                                                                             else if (LAsEgunDASuMa == "Nine")
+                            {
+                    Tổngmới = 9;
+                }
+              
+                
+                else                        if (LAsEgunDASuMa == "ten")
+                                            {
+                                                            Tổngmới = 10;
+                             }
+                
+                                         else
+                                         {
+                                                                Tổngmới = int.Parse(LAsEgunDASuMa);
+                            }
+
+                //DO YOU                KNOW THE DEFINITION                 OF INSANITY?
+
+    if                          (ПеРвАЯсУммА                      == LAsEgunDASuMa)
+                                                                                           {
+                    int 최종합계 = ((مبلغجديد + Tổngmới - Напълнете) * Füllen + Riempire * SteamGamesAndStore /Punan);    Console.WriteLine(최종합계);
+                                                                     }       //HUE HUE
+                
+         if                                                  (ПеРвАЯсУммА                     != LAsEgunDASuMa)
+                {
+                                 int দ্বিতীয়চূড়ান্তযোগফল = مبلغجديد +(Напълнете * SteamGamesAndStore /Punan * Füllen * 5 / 6 - Riempire) + Tổngmới;
+                                                                                Console.WriteLine(দ্বিতীয়চূড়ান্তযোগফল);
+                                       }
+                        }
+                  }
+                                             } }
